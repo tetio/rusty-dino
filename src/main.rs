925cc32ef2673314f914cfec3bd65c69ce2767a4
tetio <tetio@live.com>
2025-04-01 09:38:29 +0200

    rect and collisions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::render::render_resource::ShaderType;
+use bevy::render::render_resource::{AsBindGroupShaderType, ShaderType};
 use bevy::{math::prelude::*, prelude::*, window::*};
 
 const WINDOW_WIDTH: f32 = 800.;
@@ -8,6 +8,9 @@ const DINO_SIZE: f32 = 64.;
 const MOB_SIZE: f32 = 64.;
 #[derive(Component)]
 struct Dino;
+
+#[derive(Component)]
+struct DinoBox;
 
 #[derive(Component)]
 struct Mob;
@@ -26,7 +29,9 @@ struct Velocity(Vec2);
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
     mut game_state: ResMut<GameState>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     game_state.game_over = false;
 
@@ -36,26 +41,70 @@ fn setup(
     commands.spawn(Camera2d);
 
     commands.spawn((dino, Transform::from_xyz(-250., -25., 0.), Dino, Collider));
+    let mesh = meshes.add(Rectangle::new(64., 64.));
+    commands
+        .spawn((
+            Mesh2d(mesh),
+            MeshMaterial2d(materials.add(Color::srgb(255., 0., 0.))),
+            Transform::from_xyz(-250., -25., 0.),
+            DinoBox,
+        ))
+        .insert(Visibility::Hidden);
 
+    // commands.spawn((
+    //     Sprite {
+    //         image: mob_image.clone(),
+    //         flip_x: false,
+    //         ..default()
+    //     },
+    //     Transform::from_xyz(0., 0., 0.),
+    //     Mob,
+    //     Velocity(Vec2::new(-250.0, 0.)),
+    //     Collider,
+    // ));
+
+    // commands.spawn((
+    //     Sprite {
+    //         image: mob_image.clone(),
+    //         flip_x: false,
+    //         ..default()
+    //     },
+    //     Transform::from_xyz(0., 0., 0.),
+    //     Mob,
+    //     Velocity(Vec2::new(-250.0, 0.)),
+    //     Collider,
+    // ));
     commands.spawn((
         Sprite {
             image: mob_image.clone(),
-            flip_x: false,
+            flip_x: true,
             ..default()
         },
-        Transform::from_xyz(0., 0., 0.),
+        Transform::from_xyz(100., -100., 0.),
         Mob,
-        Velocity(Vec2::new(-250.0, 0.)),
+        Velocity(Vec2::new(250.0, 0.)),
         Collider,
     ));
 
     commands.spawn((
         Sprite {
-            image: mob_image,
+            image: mob_image.clone(),
             flip_x: true,
             ..default()
         },
         Transform::from_xyz(100., 0., 0.),
+        Mob,
+        Velocity(Vec2::new(250.0, 0.)),
+        Collider,
+    ));
+
+    commands.spawn((
+        Sprite {
+            image: mob_image.clone(),
+            flip_x: true,
+            ..default()
+        },
+        Transform::from_xyz(-100., 0., 0.),
         Mob,
         Velocity(Vec2::new(250.0, 0.)),
         Collider,
@@ -66,10 +115,12 @@ fn move_dino(
     mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(Entity, &mut Sprite, &mut Transform), With<Dino>>,
+    mut query_box: Query<&mut Transform, (With<DinoBox>, Without<Dino>)>,
     time: Res<Time>,
 ) {
     let mut direction = Vec3::ZERO;
     let (entity, mut sprite, mut transform) = query.single_mut();
+    let mut transform_box = query_box.single_mut();
 
     if keyboard_input.pressed(KeyCode::ArrowLeft) {
         if !sprite.flip_x {
@@ -96,8 +147,12 @@ fn move_dino(
     if keyboard_input.pressed(KeyCode::KeyV) {
         commands.entity(entity).insert(Visibility::Visible);
     }
-    transform.translation.x += direction.x * DINO_SPEED * time.delta_secs();
-    transform.translation.y += direction.y * DINO_SPEED * time.delta_secs();
+    let mov_x = direction.x * DINO_SPEED * time.delta_secs();
+    let mov_y = direction.y * DINO_SPEED * time.delta_secs();
+    transform.translation.x += mov_x;
+    transform.translation.y += mov_y;
+    transform_box.translation.x += mov_x;
+    transform_box.translation.y += mov_y;
 }
 
 fn move_mobs(mut query: Query<(&mut Sprite, &mut Transform, &mut Velocity)>, time: Res<Time>) {
@@ -118,18 +173,27 @@ fn move_mobs(mut query: Query<(&mut Sprite, &mut Transform, &mut Velocity)>, tim
 }
 
 fn collision_detection(
+    mut commands: Commands,
     dino_query: Query<&Transform, With<Dino>>,
+    dino_box_query: Query<Entity, (With<DinoBox>, Without<Dino>)>,
     mobs_query: Query<&Transform, With<Mob>>,
     mut game_state: ResMut<GameState>,
 ) {
     let dino_transform = dino_query.single();
     let dino_rect = make_rect(dino_transform, DINO_SIZE);
+    let dino_box_entity = dino_box_query.single();
+
     for mob_transform in mobs_query.iter() {
         let mob_rect = make_rect(mob_transform, MOB_SIZE);
         let ri = mob_rect.intersect(dino_rect);
         if !ri.is_empty() {
             game_state.game_over = true;
-            println!("{:?} WARNING! Collision detected!!", ri);
+            let a = commands.entity(dino_box_entity).;
+            commands.entity(dino_box_entity).insert(Visibility::Visible);
+            break;
+            // println!("{:?} WARNING! Collision detected!!", ri);
+        } else {
+            commands.entity(dino_box_entity).insert(Visibility::Hidden);
         }
     }
 }
